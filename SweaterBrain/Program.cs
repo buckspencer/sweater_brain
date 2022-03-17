b0912b9eb2b623e001b715f2b05e7664ed641b8b
Buck Spencer <buck.spencer@mutations.ltd>
2022-03-17 16:27:04 -0700

    Fix to deploy to heroku

--- SweaterBrain/Program.cs
+++ SweaterBrain/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace SweaterBrain
@@ -14,7 +15,11 @@ namespace SweaterBrain
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    _ = webBuilder.UseStartup<Startup>();
+                    var port = Environment.GetEnvironmentVariable("PORT");
+
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:" + port);
                 });
     }
 }
+               
