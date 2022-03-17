27e31def81d7e970dfab84b240c442c70fab8be3
Buck Spencer <buck.spencer@mutations.ltd>
2022-03-17 16:44:07 -0700

    Handle lint issues

--- SweaterBrain/ClientApp/src/components/Home.js
+++ SweaterBrain/ClientApp/src/components/Home.js
@@ -10,13 +10,13 @@ export class Home extends Component {
             <p>As we inevitably progress on this piece of modern ingenuity we hope to see such functional augmentations as:</p>
 
             Advanced Sweater search with:
-        <ul>
-            <li>Search by location, includes calculations incorporating cultural and temperature based sensitivities</li>
-            <li>Search by time of day</li>
-            <li>Search by event type</li>
-            <li>Randomizer, incorporates safety guardrails so that you aren't completely left in the wrong sweater. ie, a heavy sweater on a hot day.</li>
-        </ul>
-      </div>
+            <ul>
+                <li>Search by location, includes calculations incorporating cultural and temperature based sensitivities</li>
+                <li>Search by time of day</li>
+                <li>Search by event type</li>
+                <li>Randomizer incorporates safety guardrails so that you are not completely left in the wrong sweater. ie, a heavy sweater on a hot day.</li>
+            </ul>
+        </div>
     );
   }
 }
--- SweaterBrain/ClientApp/src/components/NavMenu.js
+++ SweaterBrain/ClientApp/src/components/NavMenu.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
 
+import { motion } from "framer-motion";
+import './NavMenu.css';
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
@@ -21,20 +22,36 @@ export class NavMenu extends Component {
     });
   }
 
-  render () {
+    render() {
+
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">SweaterBrain</NavbarBrand>
+            <Container>
+
+            <NavbarBrand tag={Link} to="/">
+                <motion.img
+                    whileHover={{ scale: 1.3 }}
+                    whileTap={{ scale: 0.9 }}
+                    src={require("../logo/logo.svg").default}
+                />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
+                  <NavItem>
+                    <motion.div
+                        whileHover={{ scale: 1.3 }}
+                        whileTap={{ scale: 0.9 }}>
+                        <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                    </motion.div>
+                  </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/sweater-brain">Suggester Core</NavLink>
+                    <motion.div
+                        whileHover={{ scale: 1.3 }}
+                        whileTap={{ scale: 0.9 }}>
+                        <NavLink tag={Link} className="text-dark" to="/sweater-brain">Suggester Core</NavLink>
+                    </motion.div>
                 </NavItem>
               </ul>
             </Collapse>
@@ -44,3 +61,4 @@ export class NavMenu extends Component {
     );
   }
 }
+
