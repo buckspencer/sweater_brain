f2c1f4335a124076605a4b4194799bd08204118b
Buck Spencer <buck.spencer@mutations.ltd>
2022-03-29 14:43:46 -0700

    Add GeoCode Api connection
    * Integrates with GeoCodeAPi
    * Adds tests

--- SweaterBrain/Controllers/WeatherForecastController.cs
+++ SweaterBrain/Controllers/WeatherForecastController.cs
@@ -14,18 +14,18 @@ namespace SweaterBrain.Controllers
   [Route("[controller]")]
   public class WeatherForecastController : ControllerBase
   {
+    private readonly HttpClient _httpClient;
     private readonly IConfiguration _config;
 
     public WeatherForecastController(IConfiguration config, ILogger<WeatherForecastController> _logger)
     {
       this._config = config;
+      this._httpClient = new HttpClient();
     }
 
     [HttpGet("suggester-data")]
     public Task<SuggesterDataDto> GetSuggeterData(string cityGeo)
     {
-
-      HttpClient _httpClient = new HttpClient();
 
       Task<SuggesterDataDto> returnedTemp = new SweaterService(_httpClient, _config).SuggesterData(cityGeo);
 
@@ -35,11 +35,21 @@ namespace SweaterBrain.Controllers
     [HttpGet("city-selection")]
     public List<MajorCityLocationDto> GetCitySelection()
     {
-      var _locations = new List<MajorCityLocationDto>();
-      _locations.Add(new MajorCityLocationDto("Los Angeles, Ca", new List<string> { "34", "-118" }));
-      _locations.Add(new MajorCityLocationDto("Athens, Ga.", new List<string> { "33", "-83" }));
-      _locations.Add(new MajorCityLocationDto("Bagley, MN", new List<string> { "47", "-95" }));
-      return _locations;
+        List<MajorCityLocationDto> _locations = new List<MajorCityLocationDto>
+        {
+            new MajorCityLocationDto("Los Angeles, Ca", new List<string> { "34", "-118" }),
+            new MajorCityLocationDto("Athens, Ga.", new List<string> { "33", "-83" }),
+            new MajorCityLocationDto("Bagley, MN", new List<string> { "47", "-95" })
+        };
+        return _locations;
+    }
+
+    [HttpGet("city-by-zip")]
+    public Task<MajorCityLocationDto> GetCityByZip(string postalCode)
+    {
+        Task<MajorCityLocationDto> result = new GeoService(_httpClient, _config).GeoByPostalCodeAsync(postalCode);
+
+        return result;
     }
   }
 
---
+++ SweaterBrain/Services/GeoService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SweaterBrain.Models;
+
+namespace SweaterBrain.Services
+{
+    internal class GeoService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+        private readonly string _GOOGLE_KEY;
+
+        public GeoService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+            _GOOGLE_KEY = _config["GOOGLE_KEY"];
+        }
+
+        public async Task<MajorCityLocationDto> GeoByPostalCodeAsync(string postalCode)
+        {
+            var requestUri = RequestUrl(postalCode);
+
+            using (HttpResponseMessage _response = await _httpClient.GetAsync(requestUri))
+            {
+                string _body = await _response.Content.ReadAsStringAsync();
+                GeoCodeResponse _geoCodeResponse = GeoCodeResponse.FromJson(_body);
+                MajorCityLocationDto _mappedDto = MajorCityLocationDtoFromResponse(_geoCodeResponse);
+
+                return _mappedDto;
+            }
+        }
+
+        private MajorCityLocationDto MajorCityLocationDtoFromResponse(GeoCodeResponse geoCodeResponse)
+        {
+            string _resultAddr = geoCodeResponse.Results[0].FormattedAddress;
+            double _lat = geoCodeResponse.Results[0].Geometry.Location.Lat;
+            double _lon = geoCodeResponse.Results[0].Geometry.Location.Lng;
+            MajorCityLocationDto _mapToDto = new MajorCityLocationDto(
+                _resultAddr,
+                new List<string> { _lat.ToString(), _lon.ToString() }
+            );
+
+            return _mapToDto;
+        }
+
+        private string RequestUrl(string postalCode)
+        {
+            return $"https://maps.googleapis.com/maps/api/geocode/json?sensor=true&components=country:US|postal_code:{postalCode}&key={_GOOGLE_KEY}&units=imperial";
+        }
+    }
+}
